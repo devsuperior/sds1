1a52237dec040ca92c892e05d3e1df73ad0d53ee
Washington Soares <washingtonudi@gmail.com>
2020-09-07 23:16:04 -0300

    Add Header styles

---
+++ videoaula3/Header.js
@@ -0,0 +1,27 @@
+{
+  header: {
+    paddingTop: 50,
+    height: 90,
+    backgroundColor: '#37474F',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  textLogo1: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    fontFamily: "Play_700Bold",
+    color: '#ED7947',
+    marginLeft: 10,
+    marginRight: 5,
+  },
+  textLogo2: {
+    fontWeight: 'bold',
+    fontFamily: "Play_700Bold",
+    fontSize: 18,
+    color: '#FFF'
+  },
+  tinyLogo: {
+    width: 25,
+    height: 25,
+  },
+}
